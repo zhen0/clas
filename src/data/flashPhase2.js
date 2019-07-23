5a8e2f7125bd6f9b0a560cef65b3b2a1e303e7a9
Jenny Grange <jenniferhelengrange@gmail.com>
2019-07-23 16:41:27 -0400

    adds flashcard menu

---
+++ src/data/flashPhase2.js
@@ -0,0 +1,139 @@
+export const sounds = {
+  one: [
+    {
+      id: 1,
+      word: "nail",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/snail.png",
+      sound: "s",
+      front: "s",
+      turned: true
+    },
+    {
+      id: 2,
+      word: "pple",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/apple.png",
+      sound: "a",
+      front: "a",
+      turned: true
+    },
+    {
+      id: 3,
+      word: "iger",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/tiger.png",
+      sound: "t",
+      front: "t",
+      turned: true
+    },
+    {
+      id: 4,
+      word: "late",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "p",
+      front: "p",
+      turned: true
+    }
+  ],
+  two: [
+    {
+      id: 5,
+      word: "gloo",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "i",
+      front: "i",
+      turned: true
+    },
+    {
+      id: 6,
+      word: "et",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "n",
+      front: "n",
+      turned: true
+    },
+    {
+      id: 7,
+      word: "ouse",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "m",
+      front: "m",
+      turned: true
+    },
+    {
+      id: 8,
+      word: "og",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "d",
+      front: "d",
+      turned: true
+    }
+  ],
+  three: [
+    {
+      id: 9,
+      word: "ate",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "g",
+      front: "g",
+      turned: true
+    },
+    {
+      id: 10,
+      word: "range",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "o",
+      front: "o",
+      turned: true
+    },
+    {
+      id: 11,
+      word: "at",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "c",
+      front: "c",
+      turned: true
+    },
+    {
+      id: 12,
+      word: "ey",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "k",
+      front: "k",
+      turned: true
+    }
+  ],
+  four: [
+    {
+      id: 13,
+      word: "clo",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "ck",
+      front: "ck",
+      turned: true,
+      end: true
+    },
+    {
+      id: 14,
+      word: "lephant",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "e",
+      front: "e",
+      turned: true
+    },
+    {
+      id: 15,
+      word: "mbrella",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "u",
+      front: "u",
+      turned: true
+    },
+    {
+      id: 16,
+      word: "abbit",
+      imgUrl: "https://claspics.s3.eu-west-2.amazonaws.com/plate.png",
+      sound: "r",
+      front: "r",
+      turned: true
+    }
+  ]
+};
